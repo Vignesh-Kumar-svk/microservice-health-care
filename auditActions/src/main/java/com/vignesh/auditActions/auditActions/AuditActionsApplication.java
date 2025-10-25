b0b0c177588c8af94fc294c2f25b96922585616a
Vignesh Kumar <vigneshkumaryes25@gmail.com>
2025-10-25 22:04:01 +0530

    audit Log added

---
+++ auditActions/src/main/java/com/vignesh/auditActions/auditActions/AuditActionsApplication.java
@@ -0,0 +1,13 @@
+package com.vignesh.auditActions.auditActions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditActionsApplication.class, args);
+	}
+
+}
---
+++ auditActions/src/test/java/com/vignesh/auditActions/auditActions/AuditActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vignesh.auditActions.auditActions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
